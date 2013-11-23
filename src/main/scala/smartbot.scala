ca68d916673558d01e424e9fce4c7b18924a15db
http://jayferd.us/ <jjmadkisson@gmail.com>
2013-11-22 22:53:37 -0800

    wire up a basic markov response from the bot

--- src/main/scala/smartbot.scala
+++ src/main/scala/smartbot.scala
@@ -3,30 +3,44 @@ package smartbot
 import org.jibble.pircbot._
 
 
-object SmartBot extends PircBot {
-
-  var name = "markovbot"
-
-  def main(args: Array[String]) {
-    connect("irc.freenode.net", "#csuatest")
-  }
-
-  def connect(server: String, channel:String) {
-    try {
-      setName(name)
+object SmartBot {
+  class Bot(var name: String, dict: MarkovDict, dbPath: String) extends PircBot {
+    def setup(name: String) {
       setEncoding("UTF-8")
-      connect(server)
-      joinChannel(channel) 
-    } catch { 
-      case e: NickAlreadyInUseException => {
-	name = name + "t"
-	connect("irc.freenode.net", "#csuatest")
+      setVerbose(true)
+    }
+
+    def connect(server: String, channel: String, passwordOpt: Option[String]) {
+      try {
+        setName(name)
+        setEncoding("UTF-8")
+        connect(server)
+        joinChannel(channel) 
+      } catch {
+        case e: NickAlreadyInUseException => {
+          name = name + "t"
+          connect(server, channel, passwordOpt)
+        }
       }
+    }
+
+    override def onMessage(channel: String, sender: String, login: String,
+                           hostname: String, message: String) {
+
+      sendMessage(channel, dict.generateSentence(message))
     }
   }
 
-  override def onMessage(channel: String, sender: String, login:
-			 String, hostname: String, message: String) {
-    sendMessage(channel, "testing")
+  def main(args: Array[String]) {
+    val botName = sys.env.get("BOT_NAME").getOrElse("yabishbot")
+    val channel = sys.env.get("CHANNEL").getOrElse("#csuatest")
+    val server = sys.env.get("SERVER").getOrElse("irc.freenode.net")
+    val dbPath = sys.env.get("DB_PATH").getOrElse("./db/ya_bish")
+    val passwordOpt = sys.env.get("PASSWORD")
+
+    val dict = MarkovDict.trainFromLog(dbPath)
+    val bot = new Bot(botName, dict, dbPath)
+
+    bot.connect(server, channel, passwordOpt)
   }
 }
