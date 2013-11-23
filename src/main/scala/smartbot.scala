166b2eb283b46572a5e0842747bf87d25ff6f2ea
Mary Stufflebeam <marystufflebeam@gmail.com>
2013-11-22 22:14:06 -0800

    Added nick name in use exception handling

--- src/main/scala/smartbot.scala
+++ src/main/scala/smartbot.scala
@@ -1,17 +1,28 @@
 package smartbot
 
-import org.jibble.pircbot.PircBot
+import org.jibble.pircbot._
 
 
 object SmartBot extends PircBot {
 
+  var name = "markovbot"
+
   def main(args: Array[String]) {
-    setName("stufflebotttt")
-    setVerbose(true)
-    setEncoding("UTF-8")
-    connect("irc.freenode.net")
-    identify("******")
-    joinChannel("#csuatest")
+    connect("irc.freenode.net", "#csuatest")
+  }
+
+  def connect(server: String, channel:String) {
+    try {
+      setName(name)
+      setEncoding("UTF-8")
+      connect(server)
+      joinChannel(channel) 
+    } catch { 
+      case e: NickAlreadyInUseException => {
+	name = name + "t"
+	connect("irc.freenode.net", "#csuatest")
+      }
+    }
   }
 
   override def onMessage(channel: String, sender: String, login:
