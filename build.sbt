9b03c225c2414e728caa1fda122e2aa932a6ec5b
Mary Stufflebeam <marystufflebeam@gmail.com>
2013-11-22 19:46:29 -0800

    set up pircbot

--- build.sbt
+++ build.sbt
@@ -5,5 +5,8 @@ version := "0.0.1"
 scalaVersion := "2.10.2"
 
 libraryDependencies ++= Seq(
-  "org.scala-tools.testing" %% "specs" % "1.6.9" % "test"
+  "org.scala-tools.testing" %% "specs" % "1.6.9" % "test",
+  "pircbot" % "pircbot" % "1.5.0" from "http://www.bertails.org/pircbot.jar"
 )
+
+scalacOptions ++= Seq("-encoding", "UTF8")
--- src/main/scala/smartbot.scala
+++ src/main/scala/smartbot.scala
@@ -1,10 +1,21 @@
 package smartbot
 
+import org.jibble.pircbot.PircBot
 
-object Smartbot {
 
-  def main(args: List[String]) {
-    println("Hello, world!")
+object SmartBot extends PircBot {
+
+  def main(args: Array[String]) {
+    setName("stufflebotttt")
+    setVerbose(true)
+    setEncoding("UTF-8")
+    connect("irc.freenode.net")
+    identify("******")
+    joinChannel("#csuatest")
   }
 
+  override def onMessage(channel: String, sender: String, login:
+			 String, hostname: String, message: String) {
+    sendMessage(channel, "testing")
+  }
 }
